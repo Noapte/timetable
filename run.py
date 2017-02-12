bac130b1570eff6dd83d0a0361a384228edc0447
Cezary Miacz <czarek.miacz@gmail.com>
2017-02-12 15:40:49 +0100

    Debug flag in run.py, Angular script import moved to base template

--- run.py
+++ run.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+import argparse
 from main import app
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--debug', action='store_true')
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    args = parser.parse_args()
+    app.run(debug=args.debug)
